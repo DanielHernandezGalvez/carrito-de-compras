40758953c7611350426d75b7e0b2ff0842fd9183
DanielHernandezGalvez <hernandezgalvezalejandro@gmail.com>
2022-09-12 12:03:07 -0500

    55%

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const cards = document.getElementById('cards')
 const items = document.getElementById('items')
+const footer = document.getElementById('footer')
 const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment()
 let carrito = {}
 
@@ -7,7 +11,7 @@ let carrito = {}
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
 })
-items.addEventListener('click', e => {
+cards.addEventListener('click', e => {
     addCarrito(e)  
 })
 
@@ -24,7 +28,7 @@ const fetchData = async () => {
 
 const pintarCards = data => {
     data.forEach(producto => {
-        console.log(data)
+    //    console.log(data)
         templateCard.querySelector('h5').textContent = producto.title     
         templateCard.querySelector('p').textContent = producto.precio   
         templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
@@ -32,7 +36,7 @@ const pintarCards = data => {
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     })
-    items.appendChild(fragment)
+    cards.appendChild(fragment)
 }
 
 const addCarrito = e => {
@@ -52,10 +56,19 @@ const setCarrito = objeto => {
         precio: objeto.querySelector('p').textContent,
         cantidad: 1,
     }
-    console.log(producto)
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = {...producto}
+    pintarCarrito()
+}
+
+const pintarCarrito = () => {
+    console.log(carrito)
 }
 
 /* 
 video: https://www.youtube.com/watch?v=JL7Wo-ASah4&t=1s
-minuto 37:11
+minuto 
  */
