66fc8e8e431d6421cd1a0850d39e46313aa0bde8
DanielHernandezGalvez <hernandezgalvezalejandro@gmail.com>
2022-09-12 12:43:19 -0500

    68%

--- app.js
+++ app.js
@@ -65,8 +65,32 @@ const setCarrito = objeto => {
 }
 
 const pintarCarrito = () => {
-    console.log(carrito)
+   // console.log(carrito)
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+          templateCarrito.querySelector('th').textContent = producto.id 
+          templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+          templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+          templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+          templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+          templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+          const clone = templateCarrito.cloneNode(true)
+          fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    pintarFooter()
 }
+    pintarFooter = () => {
+        footer.innerHTML = ''
+        if(Object.keys(carrito).length === 0) {
+            footer.innerHTML = '<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>'
+        }
+        const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad,0 )
+        const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad*precio,0)
+        console.log(nPrecio)
+    }
+
 
 /* 
 video: https://www.youtube.com/watch?v=JL7Wo-ASah4&t=1s
