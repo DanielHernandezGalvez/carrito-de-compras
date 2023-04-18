b16371b9db7a2748e6739647fdb5a8af6d5b86f9
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-04-18 09:46:53 -0600

    Update and rename App.js to App.ts

--- src/App.ts
+++ src/App.ts
@@ -3,15 +3,17 @@ import Container from "./Components/Container";
 import Header from "./Components/Header";
 import Welcome from "./Components/Welcome";
 import CharacterContainer from "./Components/CharacterContainer";
+import React from 'react';
+
 function App() {
-  const [characters, setCharacters] = useState(null);
-  const reqApi = async () => {
-    const api = await fetch(
-      "https://simpsons-quotes-api.herokuapp.com/quotes?count=3"
-    );
-    const frase = await api.json();
-    setCharacters(frase);
-  };
+const [characters, setCharacters] = useState<any[]>([]);
+const reqApi = async () => {
+const api = await fetch(
+"https://simpsons-quotes-api.herokuapp.com/quotes?count=3"
+);
+const frase = await api.json();
+setCharacters(frase);
+};
 
   return (
     <Container>
